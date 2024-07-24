0ed0100c52e50ee59bccc1e39b7d742e0e22b6ab
geoquimica-gomez <geoquimica.gomez@gmail.com>
2024-07-24 00:12:25 -0400

    configurando page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/desafio17Hito1",
+  base: "/desafio17Hito1A",
   plugins: [react()],
 })
